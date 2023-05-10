5d9fff087f46fcfe0b6cda15cbe40f48e0322bc5
Mya <97209406+Myapi314@users.noreply.github.com>
2023-05-10 17:11:38 -0600

    Update ArduinoController.ino for controlling 4 leds

--- ArduinoController.ino
+++ ArduinoController.ino
@@ -1,57 +1,48 @@
+#define LED_YELLOW_PIN A0
+#define LED_GREEN_PIN A1
 #define LED_RED_PIN A2
 #define LED_BLUE_PIN A3
-#define BUTTON_PIN A4
-
-byte lastButtonState = LOW;
-byte currentButtonState = LOW;
-
-unsigned long lastButtonDebounceTime = 0;
-unsigned long buttonBounceDelay = 20;
 
 void powerOffAllLEDs()
 {
+  digitalWrite(LED_YELLOW_PIN, LOW);
+  digitalWrite(LED_GREEN_PIN, LOW);
   digitalWrite(LED_RED_PIN, LOW);
   digitalWrite(LED_BLUE_PIN, LOW);
 }
 
 void setup() {
   Serial.begin(9600);
+  pinMode(LED_YELLOW_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
   pinMode(LED_RED_PIN, OUTPUT);
   pinMode(LED_BLUE_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT);
 
   powerOffAllLEDs();
 }
 
 void loop() {
-  byte readValue = digitalRead(BUTTON_PIN);
-
-  if (readValue != lastButtonState) {
-    lastButtonDebounceTime = millis();
-  }
-
-  if (millis() - lastButtonDebounceTime > buttonBounceDelay) {
-    if (readValue != currentButtonState) {
-      currentButtonState = readValue;
-      if (currentButtonState == HIGH) {
-        Serial.write(18);
-      }
-    }
-  }
-
-  lastButtonState = readValue;
-
   
   if (Serial.available() > 0) {
     int ledState = Serial.read() - '0';
 
     powerOffAllLEDs();
+    
+//    String data = Serial.readStringUntil('\n');
+//    Serial.print("You sent me: ");
+//    Serial.println(data);
 
     switch (ledState) {
       case 1:
-        digitalWrite(LED_RED_PIN, HIGH);
+        digitalWrite(LED_YELLOW_PIN, HIGH);
         break;
       case 2:
+        digitalWrite(LED_GREEN_PIN, HIGH);
+        break;
+      case 3:
+        digitalWrite(LED_RED_PIN, HIGH);
+        break;
+      case 4:
         digitalWrite(LED_BLUE_PIN, HIGH);
         break;
       default:

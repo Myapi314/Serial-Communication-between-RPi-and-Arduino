63eaba9f0b1e5e72126e4ecc7c3bf59a12bb14f6
Mya <97209406+Myapi314@users.noreply.github.com>
2023-05-10 17:13:10 -0600

    Update RPiController.py for 4 LED control thru console

--- RPiController.py
+++ RPiController.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 import serial
-import time
+
+def pick_led_state():
+    color = input('What color should I send to Arduino? ')
+    if color.lower().strip() == 'quit':
+        return -1
+    elif color.lower().strip() == 'yellow':
+        return 1
+    elif color.lower().strip() == 'green':
+        return 2
+    elif color.lower().strip() == 'red':
+        return 3
+    elif color.lower().strip() == 'blue':
+        return 4
+    else:
+        print('Not a valid color. . .')
+        return 0
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
@@ -9,10 +24,10 @@ if __name__ == '__main__':
     led_state = 0
 
     while True:
-        number = ser.read()
-        if number != b'':
-            if int.from_bytes(number, byteorder='big') == 18:
-                led_state = (led_state + 1) % 3
-                print('Button has been pressed')
-                print('Sending number ' + str(led_state) + ' to Arduino')
-                ser.write(str(led_state).encode('utf-8'))
+        led_state = pick_led_state()
+        if led_state == -1:
+            print('Quitting. . .')
+            ser.write(str(0).encode('utf-8'))
+            break
+        print('Sending number ' + str(led_state) + ' to Arduino')
+        ser.write(str(led_state).encode('utf-8'))
